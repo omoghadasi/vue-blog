722e38c75b28929235c5c11e09d8ad292a61a9c7
omid <omoghadasi@gmail.com>
2025-03-24 09:55:20 +0330

    add navbar header and alias path

--- src/page/HomePage.vue
+++ src/page/HomePage.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+import NavbarHeader from "@components/NavbarHeader.vue";
+</script>
 <template>
+  <NavbarHeader />
   <h2>home page</h2>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@page": path.resolve(__dirname, "./src/page"),
+      "@components": path.resolve(__dirname, "./src/components"),
+    },
+  },
   plugins: [vue(), tailwindcss()],
 });
